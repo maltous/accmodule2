508dbafa58778cac8e842bbf411f069ed091e99e
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2019-12-28 18:47:21 +0300

    accmodule2

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "67hvNgRFQIt":
+        Script1();
+        break;
+      case "6MxK7xz7rCy":
+        Script2();
+        break;
+      case "5dO8hcJUakR":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  window.print();
+}
+
